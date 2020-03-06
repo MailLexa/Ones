535434218fbb24d4aa1337251925a033636018a1
Your Name <ADima@mail.ru>
2020-03-06 10:02:35 +0300

    Gonki

---
+++ Gonki/src/LLL/Car.java
@@ -0,0 +1,76 @@
+package LLL;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    private static boolean winnerFound;
+    private static Lock win = new ReentrantLock();
+
+    static {
+        CARS_COUNT = 0;
+    }
+
+    private Race race;
+    private int speed;
+    private String name;
+    private int count;
+    private CyclicBarrier Cyclic1;
+    private CountDownLatch Cyclic2;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public Car(Race race, int speed, CyclicBarrier Cyclic1, CountDownLatch Cyclic2) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+        this.Cyclic1 = Cyclic1;
+        this.Cyclic2 = Cyclic2;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            Cyclic1.await();
+            Cyclic1.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            checkWinner(this);
+
+            Cyclic1.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //2 способ проверки
+    private static synchronized void checkWinner(Car c) {
+        if (!winnerFound) {
+            System.out.println(c.name + " - WIN");
+            winnerFound = true;
+        }
+    }
+}
+
+
+
+
